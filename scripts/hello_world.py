54d7c83001dc8fa433e1ba5d2df14729faa5f7a1
sara-kelemen <skk264@gmail.com>
2025-01-22 18:40:16 -0500

    Adding extra documentation to hello_world.py

--- scripts/hello_world.py
+++ scripts/hello_world.py
@@ -1,2 +1,2 @@
-"""This is a script, and as such, should only be in the scripts directory."""
+"""This is a script, and as such, should only be in the scripts directory. This is for an example."""
 print("Hello, World!")
